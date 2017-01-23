0e46964b278a6ee5401a80f273d7a04a61482014
Mathieu Parent <math.parent@gmail.com>
2017-01-23 15:47:09 +0100

    Initial PHP images

---
+++ src/families/php.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+## Copyright 2017 Mathieu Parent <math.parent@gmail.com>
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+set -e
+
+. "$PWD/src/families/common.sh"
+
+case $PHP_VERSION in
+    5.4)
+        php_dists="wheezy"
+        php_package="php5"
+        conf_dir="/etc/php5/conf.d"
+        ;;
+    5.6)
+        php_dists="jessie"
+        php_package="php5"
+        conf_dir="/etc/php5/$PHP_SAPI/conf.d"
+        ;;
+    7.0)
+        php_dists="stretch"
+        php_package="php$PHP_VERSION"
+        conf_dir="/etc/php/$PHP_VERSION/$PHP_SAPI/conf.d"
+        ;;
+    *)
+        echo "ERROR: Mandatory variable is not correct: PHP_VERSION=$PHP_VERSION"
+        exit 1
+        ;;
+esac
+
+case $PHP_SAPI in
+    apache2)
+        php_package="apache2 libapache2-mod-$php_package"
+        php_port=80
+        php_cmd="[\"apache2\", \"-DFOREGROUND\"]"
+        ;;
+    fpm)
+        php_package="$php_package-$PHP_SAPI"
+        php_port=9000
+        php_cmd="[\"$php_package\"]"
+        ;;
+    *)
+        echo "ERROR: Mandatory variable is not correct: PHP_SAPI=$PHP_SAPI"
+        exit 1
+        ;;
+esac
+
+dockerfile_packages="$dockerfile_packages $php_package"
+php_short="$PHP_VERSION-$PHP_SAPI"
+
+dockerfile_path=php/$php_short$onbuild_short/Dockerfile
+docker_tag=nantesmetropole/php:$php_short$onbuild_short
+
+dockerfile_generate_run_cont() {
+    cat <<EOF >> "$dockerfile_path"
+    rm -v $conf_dir/*
+
+USER www-data
+
+EXPOSE $php_port
+
+WORKDIR /var/www/html
+
+CMD $php_cmd
+EOF
+}
