1e83a833b6c02536e229b06e46dd71cc559009f7
Mathieu Parent <math.parent@gmail.com>
2017-05-02 15:51:38 +0200

    Initial SSH client image

---
+++ spec/families/ssh_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+describe "ssh" do
+  before(:all) do
+    @docker_image_tag = "nantesmetropole/ssh:latest"
+
+    opts = {
+      'Image'      => @docker_image_tag,
+      'OpenStdin'  => true,
+      'StdinOnce'  => true,
+      'Cmd'        => ['/bin/sh', '-c', 'read wait'],
+      'HostConfig' => {
+      },
+    }
+    @container = ::Docker::Container.create(opts)
+    @container.start
+    while @container.json['State'].key?('Health') && @container.json['State']['Health']['Status'] == "starting" do
+      sleep 0.5
+    end
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_container, @container.id
+  end
+
+  after(:all) do
+    @container.stop
+    @container.delete
+  end
+
+  # SSH tests
+  it "has ssh in PATH" do
+    expect(command('ssh').exit_status).to eq(255)
+  end
+
+  it "user is nobody" do
+    expect(command('id -un').stdout.strip).to eq('nobody')
+  end
+end
